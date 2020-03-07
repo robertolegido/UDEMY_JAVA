aeaa8dbee43c5009056f9587325a2966e27ba6f9
robertols <roberto.legidosilva@gmail.com>
2020-03-07 19:02:28 +0100

    Añadida clase TipoChar

--- HolaMundo/src/main/java/TipoChar.java
+++ HolaMundo/src/main/java/TipoChar.java
@@ -1,10 +1,10 @@
 public class TipoChar {
     public void main(String[] args){
-        float floatVar = 10340;
-        System.out.println(floatVar);
-        System.out.println("bits tipo por float " + Float.SIZE);
-        System.out.println("bytes tipo por float " + Float.BYTES);
-        System.out.println("Valor minimo por float "+ Float.MIN_VALUE);
-        System.out.println("Valor minimo por float "+ Float.MAX_VALUE);
+        // float floatVar = 10340;
+        // System.out.println(floatVar);
+        System.out.println("bits tipo por float " + Character.SIZE);
+        System.out.println("bytes tipo por float " + Character.BYTES);
+        System.out.println("Valor minimo por float "+ Character.MIN_VALUE);
+        System.out.println("Valor minimo por float "+ Character.MAX_VALUE);
     }
 }

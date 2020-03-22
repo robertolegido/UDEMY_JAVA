baa409bf78b6969e7d21fa6b84be9c6a7de3db6a
robertols <roberto.legidosilva@gmail.com>
2020-03-22 11:37:25 +0100

    Añadida clase de converisones junto a Scanner (mac)

--- HolaMundo/src/main/java/ConversionTipos.java
+++ HolaMundo/src/main/java/ConversionTipos.java
@@ -3,6 +3,12 @@ public class ConversionTipos {
         // De string a int
         var edad = Integer.parseInt("20");
         System.out.println("La edad es: " + edad);
-
+        // De string a double
+        double varPi = Double.parseDouble("3.1416");
+        System.out.println("El valor es: " + varPi);
+        // El tipo char tiene una unica excepción. El char solo puede almacenar un caracter
+        // Se puede asociar una posicion del array de un string
+        char c = "hola".charAt(1); // segundo caracter
+        System.out.println("Valor del caracter c es: " + c);
     }
 }
